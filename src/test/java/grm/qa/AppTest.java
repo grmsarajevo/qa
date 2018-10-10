5033929373c5d83360e535a135e05800f80cdb56
kenanr <kenan.rizvanovic@green-river-media.com>
2018-10-10 10:27:21 +0200

    first

---
+++ src/test/java/grm/qa/AppTest.java
@@ -0,0 +1,14 @@
+package grm.qa;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+	@Test
+    public void test() {
+    	System.out.println("Helloooooo");
+    }
+}
---
+++ src/test/java/grm/qa/test1.java
@@ -0,0 +1,14 @@
+package grm.qa;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	public void Testing() {
+		
+		System.out.println("test1");
+		
+	}
+
+}
